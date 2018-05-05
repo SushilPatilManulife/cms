b447ae286bae450f390a342fc2854f1aa4e65a0d
sushilApp <patilsushil1991@gmail.com>
2018-05-05 18:02:08 -0400

    Project Structure

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>WebIndiaBiz</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+        <h1>Web India Biz coming soon</h1>
+  </body>
+</html>
